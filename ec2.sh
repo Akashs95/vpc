d09b4f915b9c381ead9669e78eba88be20202d29
akash <sonawaneaakash56@gmal.com>
2025-01-04 13:47:25 +0530

    test

--- ec2.sh
+++ ec2.sh
@@ -2,4 +2,4 @@
 echo "entered into shell"
 echo "creating cloudformation stack"
 echo " this stack will create s3 bucket"
-aws cloudformation deploy --template-file /01a-S3.yml --stack-name test-stack-vpc-core --parameter-overrides Key1=Value1 Key2=Value2 --tags Key1=Value1 Key2=Value2 --region eu-west-1
+aws cloudformation deploy --template-file 01a-S3.yml --stack-name test-stack-vpc-core --parameter-overrides Key1=Value1 Key2=Value2 --tags Key1=Value1 Key2=Value2 --region eu-west-1
